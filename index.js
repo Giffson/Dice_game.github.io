5642949a157b6ccb6b614be09ce57d246a11dccf
Giffson <92511085+Giffson@users.noreply.github.com>
2021-11-12 00:19:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function myfunction() {
   // Dice1
   var randomNumber1=Math.floor(Math.random()*6+1);
-  var randomDiceimage1="Dice"+randomNumber1+".png";
+  var randomDiceimage1="dice"+randomNumber1+".png";
   var randomImagesource1="images/"+randomDiceimage1;
   var image1=document.querySelectorAll("img")[0];
   image1.setAttribute("src",randomImagesource1);
   // Dice2
   var randomNumber2=Math.floor(Math.random()*6+1);
-  var randomDiceimage2="Dice"+randomNumber2+".png";
+  var randomDiceimage2="dice"+randomNumber2+".png";
   var randomImageSource2="images/"+randomDiceimage2;
   var image2=document.querySelectorAll("img")[1];
   image2.setAttribute("src",randomImageSource2);
